fd96b015005e3c8ea725c637355c383c7ac6b4c7
eddiegemayel <eddiegem16@gmail.com>
2013-09-04 20:57:37 -0400

    changed salary code

--- activities/JS/initial.js
+++ activities/JS/initial.js
@@ -1,11 +1,13 @@
 
 
 //Salary Math
-var salary=10000;
+var salary=100;
 var raise=.05;
 var yearOne=salary*raise+salary;
 var yearTwo=yearOne*.05+yearOne;
 var yearThree=yearTwo*.05+yearTwo;
+
+
 
 console.log(yearOne +' Year One');
 console.log(yearTwo +' Year Two');
