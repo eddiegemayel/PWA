4aec91c55dd3096d6db40a8cf4be72d301ee6096
eddiegemayel <eddiegem16@gmail.com>
2013-09-10 14:04:30 -0400

    Pizza Party
    added first variables

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,9 @@
 // Eddie Gemayel, September 10 2013, expressions assignment
 
 
+
+//Sparky the dog
+
 var age = 2;           //variable tells age in human years
 var dogYears = age * 7;	// variable tells age in dog years, which is age times 7
 
@@ -9,3 +12,9 @@ console.log("Sparky is " + age +" human years old which is "+ dogYears +" in dog
 // code above displays sparkys age
 
 
+
+//Slice of Pie I
+
+var slices = 6;
+var people = 8;
+var pizzas = 4;
