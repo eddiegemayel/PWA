f059a071912a6d678d7a9b063d73bfc6fe42a50d
eddiegemayel <eddiegem16@gmail.com>
2013-09-10 14:10:51 -0400

    Slice of Pie part I
    completed part one of activity. Added comments and eveverything

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -15,6 +15,13 @@ console.log("Sparky is " + age +" human years old which is "+ dogYears +" in dog
 
 //Slice of Pie I
 
-var slices = 6;
-var people = 8;
-var pizzas = 4;
+var slices = 6;  //this variable describes how many slices are per pizza
+var people = 8;	 // this variable tells how many people are partying
+var pizzas = 4;	// this variable tells us how many pizzas are present
+
+var perPerson = (slices * pizzas) / people; // simple math to tell us how many slices per guy
+
+console.log("Each person ate "+ perPerson + " slices of pizza at the party.");
+
+//code above displays math and how much slices each person recieves
+
