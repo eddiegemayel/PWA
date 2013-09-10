c821e08c0b3bcbcd53d83d4ae35ffc891bb65b81
eddiegemayel <eddiegem16@gmail.com>
2013-09-10 14:01:50 -0400

    Sparky the Dog
    Added comments.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,4 +1,6 @@
-var age = 2;
-var dogYears = age * 7;
+var age = 2;           //variable tells age in human years
+var dogYears = age * 7;	// variable tells age in dog years, which is age times 7
 
 console.log("Sparky is " + age +" human years old which is "+ dogYears +" in dog years.");
+
+// code above displays sparkys age
