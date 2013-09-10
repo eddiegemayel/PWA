61b690b4829bbb9820ccef105941cabe22e1ff03
eddiegemayel <eddiegem16@gmail.com>
2013-09-10 14:02:52 -0400

    added discriptive comments to the top of the page

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,11 @@
+// Eddie Gemayel, September 10 2013, expressions assignment
+
+
 var age = 2;           //variable tells age in human years
 var dogYears = age * 7;	// variable tells age in dog years, which is age times 7
 
 console.log("Sparky is " + age +" human years old which is "+ dogYears +" in dog years.");
 
 // code above displays sparkys age
+
+
