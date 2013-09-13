7249969ae708c639b1be6b701a9acf213a45054e
eddiegemayel <eddiegem16@gmail.com>
2013-09-13 19:15:42 -0400

    if statements yo

--- activities/day2/js/script.js
+++ activities/day2/js/script.js
@@ -1,10 +1,19 @@
 console.log("started");
 
-var testGrade = 87;
+var testGrade = 50;
 
 if (testGrade>90){
 	console.log("You got an A.");
 }
 else if (testGrade > 80){
 	console.log("You got a B.");
+}
+else if(testGrade > 70){
+	console.log("You got a C.");
+}
+else if(testGrade > 60){
+	console.log("You got a D.");
+}
+else{
+	console.log("You failed!");
 }
