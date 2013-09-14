ec6675193023aee66f50bd0a1dae50517af0bc0c
eddiegemayel <eddiegem16@gmail.com>
2013-09-13 21:53:11 -0400

    Last chance for gas!
    Added comments to my code. all works. moving on to next activity

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -2,14 +2,14 @@
 
 // Last chance for Gas
 
-var MPG = 20; 
-var gallons = 8;
-var distance = MPG * gallons;
+var MPG = 20; 						//MPG the car gets
+var gallons = 8;					//How many gallons are in car
+var distance = MPG * gallons;		//distance you can travel before running on empty
 
 if (distance >200){
-	console.log("You can Make it through the next 200 miles!");
-}
+	console.log("You can Make it through the next 200 miles!");		
+}		//if the distance is more than 200 miles, you're good
 else{
 	console.log("You can only make it the next "+ distance+" miles! Better re-fuel son.");
-}
+}		//if the distance is less than 200, better get gas so you dont die!
  
