d1e1ed5147c2aee85ada5c1ad2ad13043f7b6632
eddiegemayel <eddiegem16@gmail.com>
2013-09-13 21:50:40 -0400

    Last chance for gas
    Wrote if statement and else statement for this problem. about to
    comment my thoughts into code

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,2 +1,15 @@
 //Eddie Gemayel , Friday Sept 13. Conditionals worksheet
 
+// Last chance for Gas
+
+var MPG = 20; 
+var gallons = 8;
+var distance = MPG * gallons;
+
+if (distance >200){
+	console.log("You can Make it through the next 200 miles!");
+}
+else{
+	console.log("You can only make it the next "+ distance+" miles! Better re-fuel son.");
+}
+ 
