b0e4472c5f577e4e06dafbbbcbc70a34a346ef41
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 17:46:26 -0400

    Conditionals (Personal)
    added other variables, uncle, aunt, and parents.

--- assignments/Conditionals_Personal/js/script.js
+++ assignments/Conditionals_Personal/js/script.js
@@ -3,3 +3,7 @@
 //Enough birthday money to buy a new phone?
 
 var granny=prompt("Enter how much money your grandmother gave you for your birthday.: ");
+
+var parents=prompt("Enter how much money your parents gave you for your birthday: ");
+
+var uncle=prompt("Enter how much money your uncle and aunt gave you for your birthday: ");
