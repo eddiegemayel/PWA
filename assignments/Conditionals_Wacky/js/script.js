54da637a60218440f07c05de76e453f81ad82a48
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 17:30:18 -0400

    Conditinals (Wacky)
    added the other prompts for the other grades.

--- assignments/Conditionals_Wacky/js/script.js
+++ assignments/Conditionals_Wacky/js/script.js
@@ -4,6 +4,8 @@
 //Average of your exams
 
 var grade1=prompt("Please enter your grade on the first exam: ");
+var grade2=prompt("Please enter your grade on the second exam: ");
+var grade3=prompt("Please enter your grade on the third exam: ");
 
  
  
