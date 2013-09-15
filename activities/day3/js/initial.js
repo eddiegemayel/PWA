ce9b724bd0a37cdf27bc3dc2ee3c0f568a9f976f
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 13:44:01 -0400

    made if statement in class

--- activities/day3/js/initial.js
+++ activities/day3/js/initial.js
@@ -1,5 +1,17 @@
 var num = 10+"";
 var string="happy";
 var num2 =15;
-var concat = num + num2;
+var concat = num*1 + num2;
 console.log(concat);
+
+var score = 0;
+
+if(score >0){
+	console.log("You win!");
+}
+else if (score ==0){
+	console.log("Did you even play?");
+}
+else{
+	console.log("You suck!");
+}
