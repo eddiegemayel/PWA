702b604638c6b32dd7d2e9ec4d58d59ae0bd96f5
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 18:08:10 -0400

    Conditionals (Industry)
    added if statements. everything seems to be working good. going to add
    the profit margin to appear in the console logs

--- assignments/Conditionals_Industry/js/script.js
+++ assignments/Conditionals_Industry/js/script.js
@@ -9,3 +9,13 @@ var shoeCost= 10;
 var profit=prompt("How much money do you need to make today to be profitable? ");
 
 var total=shoesSold*shoeCost;
+
+
+if(total > profit){
+	console.log("Wow! You made $"+ total +" today, and you only had to make more than $"+profit+". You were profitable!");
+
+}
+
+else if(total <= profit){
+	console.log("You only made $"+ total +" today. You fail.");
+}
