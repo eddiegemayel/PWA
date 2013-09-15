5e2635568f56146d93eff1116c04e73cbf64b901
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 17:44:46 -0400

    Conditionals (Personal)
    first variable asks for how much you got from your grandma

--- assignments/Conditionals_Personal/js/script.js
+++ assignments/Conditionals_Personal/js/script.js
@@ -1,3 +1,5 @@
 //Eddie Gemayel , Sunday Sept 14. Conditionals Personal worksheet
  
 //Enough birthday money to buy a new phone?
+
+var granny=prompt("Enter how much money your grandmother gave you for your birthday.: ");
