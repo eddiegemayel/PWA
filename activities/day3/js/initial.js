cec82e66d1a1ba63886b2844bdfa929a1d1c5a39
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 15:07:39 -0400

    did shuttle countdown loop

--- activities/day3/js/initial.js
+++ activities/day3/js/initial.js
@@ -1,25 +1,45 @@
-var num = 10+"";
-var string="happy";
-var num2 =15;
-var concat = num*1 + num2;
-console.log(concat);
 
-var score = 0;
 
-if(score >0){
-	console.log("You win!");
+//array inception
+
+var newArray =[];
+newArray[0]=["pear", "apple", "grape"];
+newArray[1]=["bat", "cat", "rat", "frog"];
+newArray[2]=["Joe", "sae", "Apollo", "Starbuck", "Malik"];
+
+console.log(newArray[0][2] );
+newArray[3]=[];
+newArray[3] [0]="Monkey";
+
+console.log(newArray[3] [0]);
+
+var loopingArray=[];
+
+//for(i=0;				//initial variable and value
+//	i<=10;				//number of times to do the loop
+//	i++){				//what to do to the initial value after loop is finished
+	
+	//stuff to be done
+//	console.log(i);
+//	loopingArray[i]=[];
+//	loopingArray[i][0]="pos " + i;
+//	console.log(loopingArray [i] [0]);
+	
+//}
+
+
+
+//Shuttle launch
+var practiceArray=[];
+
+for(i=10; i>=0; i--){
+
+	//console.log(i);
+	practiceArray[i]=[];
+	practiceArray[i][0]="Countdown: " + i;
+	console.log(practiceArray [i] [0]);
+	
+	
+
 }
-else if (score ==0){
-	console.log("Did you even play?");
-}
-else{
-	console.log("You suck!");
-}
-
-
-var gamesArray =["GTA", "FIFA", "Fallout"];
-
-console.log(gamesArray[0]);
-gamesArray.push("Saints Row");
-console.log(gamesArray[3]);
-
+console.log("Blastoff!");
