6bd0eb8ce6ed7e3ed160db5d8d5c755e36bb4df5
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 18:03:36 -0400

    Conditionals (Industry)
    added variables neccessary for assignment. includes shoes sold, profit
    threshold, and the set cost of each pair of shoes

--- assignments/Conditionals_Industry/js/script.js
+++ assignments/Conditionals_Industry/js/script.js
@@ -2,4 +2,8 @@
 
 //Number of shoes sold
 
-var shoesSold=prompt("Enter the amount of shoes sold today: ");
+var shoesSold=prompt("Enter the amount of pairs of shoes sold today: ");
+
+var shoeCost= 10;
+
+var profit=prompt("How much money do you need to make today to be profitable? ");
