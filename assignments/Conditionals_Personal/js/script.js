66e972e54886e6cb11fcb2160d4e412ef238f4ce
eddiegemayel <eddiegem16@gmail.com>
2013-09-15 17:49:00 -0400

    Conditionals (Personal)
    added phone cost variable. a new phone is $300!! damn

--- assignments/Conditionals_Personal/js/script.js
+++ assignments/Conditionals_Personal/js/script.js
@@ -8,4 +8,11 @@ var parents=prompt("Enter how much money your parents gave you for your birthday
 
 var uncle=prompt("Enter how much money your uncle and aunt gave you for your birthday: ");
 
-var total= uncle+parents+granny;
+var totalCost= uncle+parents+granny;
+
+var phoneCost= 300;
+
+
+if(totalCost){
+	
+}
