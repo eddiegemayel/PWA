87cb75ac4d7782ef630504d76c8764d905e1a618
eddiegemayel <eddiegem16@gmail.com>
2013-09-16 21:45:05 -0400

    Circumference of a Circle
    Got ahead of myself. Wrote function to find circumference. tested and
    works

--- assignments/Functions_worksheet/js/script.js
+++ assignments/Functions_worksheet/js/script.js
@@ -1,4 +1,15 @@
 // Eddie Gemayel, September 16 2013, functions Worksheet
 
 
+//Circumference 
+//Formula = C=d*pi  or 2*pi*r=C
+
+function circumference (){
+	var radius = 6;
+	var pi = 3.14;
+	var answer = 2*pi*radius;
+	return answer;
+}
+console.log(circumference());
+
 
