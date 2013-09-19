d471117a6b3ccf2122e041cb81b0b6ee1f8140c4
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 21:53:15 -0400

    Functions Personal
    Adding if statements in the function to find out of the player is
    considered a "pro" at super mario

--- assignments/Function_Personal/js/script.js
+++ assignments/Function_Personal/js/script.js
@@ -12,10 +12,23 @@ var score3 = prompt("Enter the third Score you got on Super Mario Bros: ");
 function calcScore (){
 
 	var avgScore ;
+	var result = "";
+	
+	
 	avgScore = (score1* score2* score3) / 3;
 	return avgScore;
 
-
+	if(avgScore > 10000){
+		result = "Wow! You are a pro! With a score of "+ avgScore + "!";
+	
+	}
+	else if(avgScore == 10000){
+		result = "Right at 10,000? Interesting Score.";
+	
+	}
+	else(){
+		result = "You fail! Your score was only "+ avgScore + "!";
+	}
 }
  
  
