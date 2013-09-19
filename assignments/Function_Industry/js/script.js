70d2f6ff35e07d663961df10018d9f215de3182f
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 21:27:17 -0400

    Function Industry
    Going to prompt the user for values of the cube. Function is complete
    and worked when tested with hard coded numbers

--- assignments/Function_Industry/js/script.js
+++ assignments/Function_Industry/js/script.js
@@ -4,9 +4,14 @@
 //Finding the Volume of a Cube
 //Length * Width * Height
 
+var height = prompt("Enter the height of the cube.");
+
+
 function volume (height, width, length){
-		
+		var calcVolume ;
+		calcVolume = (height * width * length);
+		return calcVolume
 
 }
  
- 
+//console.log(volume(20, 10, 5));
