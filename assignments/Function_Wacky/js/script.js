cfd3be4f9b8cdcb1f41cd8ee89133a73f5a65a64
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 22:16:29 -0400

    Functions Wacky
    variable added for the result. added ternary if statement. If you are
    17 and above you can see the movie. if not, you cant see the movie.

--- assignments/Function_Wacky/js/script.js
+++ assignments/Function_Wacky/js/script.js
@@ -9,6 +9,9 @@ var dob = prompt("Please enter your date of birth : ");
 function oldEnough (birthYear){
 		var currentYear = 2013;
 		var age = currentYear - birthYear;
+		var result ="";
+		
+		result = (age >= 17) ? "You can see this R rated movie since you are "+ age +"." : "You cannot see this R rated movie since you are the childish age of "+ age +"." ; 
 
 }
  
