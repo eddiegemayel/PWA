2ee2dc796686c5f4cc1e76c90bbc00d3409cb2a3
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 21:06:34 -0400

    file rearrange
    rearranging files for Functions Assignment

--- lecture activities/day4/js/initial.js
+++ lecture activities/day4/js/initial.js
@@ -116,14 +116,15 @@ for(outer=0; outer<10; outer++){
 
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////////
 //step one: sort the world alphabetically so it appears in blocks and chunkss
 //
-//step two: prompt for x and y coordinate to see where u r
+//step two: prompt for [x] and [y] coordinate to see where u r. Prompt then if statement
 //
 //step three: start process of making it a game
-
-
+//
+//step four: Edges. you can stop player or teleport them to opposite side of the map
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 // function that cubes a value
--- lecture activities/day5/js/initial.js
+++ lecture activities/day5/js/initial.js
@@ -1,4 +1,4 @@
 //lecture5
 
-//do while
+
 
