f8ab20be9aa2bbe903e6f2967d442dbabf8a3368
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 21:28:13 -0400

    Function Industry
    asking the user for the width of the cube

--- assignments/Function_Industry/js/script.js
+++ assignments/Function_Industry/js/script.js
@@ -4,7 +4,8 @@
 //Finding the Volume of a Cube
 //Length * Width * Height
 
-var height = prompt("Enter the height of the cube.");
+var height = prompt("Enter the height of the cube: ");
+var width = prompt("Enter the width of the cube: ");
 
 
 function volume (height, width, length){
