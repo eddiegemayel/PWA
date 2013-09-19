dd8664d988d71002512e8c202038664840e1eb4f
eddiegemayel <eddiegem16@gmail.com>
2013-09-18 21:34:09 -0400

    Function Industry
    Added comments to code. Moving on to next Function assignment

--- assignments/Function_Industry/js/script.js
+++ assignments/Function_Industry/js/script.js
@@ -4,16 +4,26 @@
 //Finding the Volume of a Cube
 //Length * Width * Height
 
-var height = prompt("Enter the height of the cube: ");
-var width = prompt("Enter the width of the cube: ");
-var length = prompt("Enter the length of the cube: ");
+var height = prompt("Enter the height of the cube: ");	//variable holds height of cube
+var width = prompt("Enter the width of the cube: ");	//var holds width given by user
+var length = prompt("Enter the length of the cube: ");	//var holds length of cube given by user
 
 
-function volume (height, width, length){
-		var calcVolume ;
-		calcVolume = (height * width * length);
-		return calcVolume
+
+
+//function named volume, with	paramaters height width and length
+
+function volume (height, width, length){		
+
+		var calcVolume ;	//variable holds the volume of the cube
+		
+		calcVolume = (height * width * length);	//calculates the volume
+		
+		return calcVolume;				//returns the value to us
+		
 
 }
  
-console.log(volume(height, width, length));
+console.log("The volume is "+ volume(height, width, length));
+
+//console log what the volume of the given cube is
