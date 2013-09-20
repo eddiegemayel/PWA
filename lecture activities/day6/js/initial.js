1f473e2cd3d8005f004260c8c458c1b36b2ec92d
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 19:37:02 -0400

    added other do while loop

--- lecture activities/day6/js/initial.js
+++ lecture activities/day6/js/initial.js
@@ -5,24 +5,29 @@
 var initPass="";
 var secondPass="";
 var validLength = false;
+var validMatch=false;
+
+do{
 
 
-do {
-	initPass = prompt("Please Enter a password (min:8 max: 12)");
-	if(initPass.length>=8 && initPass.length <=12){
-		validLength=true;
+	do {
+		initPass = prompt("Please Enter a password (min:8 max: 12)");
+		if(initPass.length>=8 && initPass.length <=12){
+			validLength=true;
+		}
+
+
 	}
+	while(validLength=false);
 
 
-}
-while(validLength=false);
+	secondPass = prompt("Please re-enter password: ");
 
-
-secondPass = prompt("Please re-enter password: ");
-
-if(secondPass==initPass){
-	console.log("They match!");
-}
-else{
-	console.log("They do not match.");
-}
+	if(secondPass==initPass){
+		console.log("They match!");
+		validMatch=true;
+	}
+	else{
+		console.log("They do not match.");
+	}
+}while (validMatch==false)
