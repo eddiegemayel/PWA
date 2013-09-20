59a7fee51d0bc0db340047898615224409f76058
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 19:20:01 -0400

    added do while loop

--- lecture activities/day6/js/initial.js
+++ lecture activities/day6/js/initial.js
@@ -2,5 +2,18 @@
 
 //password problem
 
+var initPass="";
+var secondPass="";
+var validLength = false;
+
+
+do {
+	initPass = prompt("Please Enter a password (min:8 max: 12)");
+	
+
+
+
+}
+while(validLength=false);
 
 
