02e9b7334d0cb35c90e54326a3e9069a430084c4
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 21:52:58 -0400

    Expressions Personal
    Making a simple expression using an Array to determine if you have
    enough money to purchase all the items in your shopping cart while on
    Amazon . com

--- assignments/Expressions_Personal/js/initial.js
+++ assignments/Expressions_Personal/js/initial.js
@@ -1,4 +1,10 @@
 //Eddie Gemayel. Friday Sept 20. Expressions Personal
 
+//Calculates if you have enough money to buy all the items you want on Amazon
+
+var shoppingCartItems = [];
+shoppingCartItems[0]=prompt("Enter the cost of the first item in your shopping cart: ");
+
+
 
 
