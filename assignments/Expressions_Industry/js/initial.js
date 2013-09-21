db46f1ac4450f20c9903e6cbd598a02cf1322682
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 21:26:13 -0400

    Expressions Industry
    Third and final variable added. Asks how many hours a week you work,
    parse int makes sure its an integer thats added

--- assignments/Expressions_Industry/js/initial.js
+++ assignments/Expressions_Industry/js/initial.js
@@ -6,5 +6,6 @@ var companyName = prompt("Enter the name of the company you work for: ");
 
 var hourlyWage = parseInt(prompt("Enter the amount you get paid per hour: "));
 
+var hoursWorked = parseInt(prompt("Enter the amount of hours you work per week: "));
 
 
