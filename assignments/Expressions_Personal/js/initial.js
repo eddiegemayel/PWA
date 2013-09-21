fde7617e5e90240348267a9b03a124968814dce3
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 22:05:56 -0400

    Expressions Personal
    Added parseInt to the prompt to make sure they are numbers. Tested code
    and worked. Going in to add comments

--- assignments/Expressions_Personal/js/initial.js
+++ assignments/Expressions_Personal/js/initial.js
@@ -3,10 +3,10 @@
 //Calculates if you have enough money to buy all the items you want on Amazon
 
 var shoppingCartCost = [];
-shoppingCartCost[0] = prompt("Enter the cost of the first item in your shopping cart: ");
-shoppingCartCost[1] = prompt("Enter the cost of the second item in your shopping cart: ");
-shoppingCartCost[2] = prompt("Enter the cost of the third item in your shopping cart: ");
-shoppingCartCost[3] = prompt("Enter the cost of the fourth item in your shopping cart: ");
+shoppingCartCost[0] = parseInt( prompt("Enter the cost of the first item in your shopping cart: "));
+shoppingCartCost[1] = parseInt(prompt("Enter the cost of the second item in your shopping cart: "));
+shoppingCartCost[2] = parseInt(prompt("Enter the cost of the third item in your shopping cart: "));
+shoppingCartCost[3] = parseInt(prompt("Enter the cost of the fourth item in your shopping cart: "));
 
 var budget = parseInt(prompt("Enter how much you can spend without going broke: "));
 
@@ -14,6 +14,11 @@ var totalCost = shoppingCartCost[0] + shoppingCartCost[1] + shoppingCartCost[2] 
 
 if(totalCost>budget){
 	console.log("You can't afford what you want to buy! You want to spend $"+ totalCost +" but you only have $"+ budget +"! Haha!");
+}
+else{
+	console.log("You can afford what you want to buy! You want to spend $" + totalCost + " and you have $"+ budget +" to spend!");
+
+
 }
 
 
