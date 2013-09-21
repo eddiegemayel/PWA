f2c3e0da2d9637591aba550851ed9ca7952de8b8
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 22:23:16 -0400

    Expressions Personal
    first variable asks first armor value

--- assignments/Expressions_Wacky/js/initial.js
+++ assignments/Expressions_Wacky/js/initial.js
@@ -2,5 +2,7 @@
 
 //Adding up armor values in a video game to see if you are ready for a boss fight or not
 
+var headArmor = parseInt(prompt("Enter the armor value of your equipped helmet(10-100): "));
+
 
 
