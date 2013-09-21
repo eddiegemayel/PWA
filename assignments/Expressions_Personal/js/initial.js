323af616bb228d4c34a91dd981a33e50dc97ca77
eddiegemayel <eddiegem16@gmail.com>
2013-09-20 21:55:47 -0400

    Expressions Personal
    added Array to hold cost of each item in shopping cart. each item is
    prompted for the user to enter how much each item is

--- assignments/Expressions_Personal/js/initial.js
+++ assignments/Expressions_Personal/js/initial.js
@@ -2,8 +2,13 @@
 
 //Calculates if you have enough money to buy all the items you want on Amazon
 
-var shoppingCartItems = [];
-shoppingCartItems[0]=prompt("Enter the cost of the first item in your shopping cart: ");
+var shoppingCartCost = [];
+shoppingCartCost[0] = prompt("Enter the cost of the first item in your shopping cart: ");
+shoppingCartCost[1] = prompt("Enter the cost of the second item in your shopping cart: ");
+shoppingCartCost[2] = prompt("Enter the cost of the third item in your shopping cart: ");
+shoppingCartCost[3] = prompt("Enter the cost of the fourth item in your shopping cart: ");
+
+
 
 
 
