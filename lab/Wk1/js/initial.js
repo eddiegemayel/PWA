bf573f30090f44bc8ae2119200f65d42e43d6f3e
eddiegemayel <eddiegem16@gmail.com>
2013-10-01 15:22:19 -0400

    Duel 1
    Added function with loop to start rounds. Adding fucntion that
    determines the winner of the duel

--- lab/Wk1/js/initial.js
+++ lab/Wk1/js/initial.js
@@ -8,8 +8,38 @@ var player2Name = "Batman";
 var player1Health = 100;
 var player2Health = 100;
 
-var player1Damage = Math.random();
-var player2Damage = Math.random();
 
+
+var round = 0;
+
+function fight(){
+	alert(player1Name +":"+ player1Health +"  START  "+ player2Name +":"+player2Health);
+	
+	for(i=0; i<=10; i++){
+		var player1Damage = Math.floor(Math.random());
+		var player2Damage = Math.floor(Math.random());
+		var player1Health =  player1Health - player2Damage;
+		var player2Health = player2Health - player1Damage;
+		
+		
+	
+	}
+	
+	function winnerCheck(){
+		var result ="";
+		
+		if(player1Health > player2Health){
+			result = player1Name + " WINS!";
+		}
+		else if(player1Health < player2Health){
+			result = player2Name + " WINS!";
+		}
+		else if( player1Health < 0 && player2Health < 0){
+			result = "You both DIE!";
+		}
+		return result;
+	}
+	
+}
 
 
