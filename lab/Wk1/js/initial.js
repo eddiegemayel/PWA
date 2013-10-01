296f8b5dc50fce891c90e644ea819c88f5a1f0b2
eddiegemayel <eddiegem16@gmail.com>
2013-10-01 15:07:44 -0400

    Duel 1
    added the player variables

--- lab/Wk1/js/initial.js
+++ lab/Wk1/js/initial.js
@@ -2,5 +2,14 @@
 //Eddie Gemayel , Tuesday October 1 2013. Duel part 1
 
 
+var player1Name = "Spiderman";
+var player2Name = "Batman";
+
+var player1Health = 100;
+var player2Health = 100;
+
+var player1Damage = Math.random();
+var player2Damage = Math.random();
+
 
 
