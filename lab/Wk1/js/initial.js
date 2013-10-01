b0079f6740d40c83e00fc50e2bb3a480acd9c8b3
eddiegemayel <eddiegem16@gmail.com>
2013-10-01 15:48:03 -0400

    Duel 1
    Messed around with the math.floor and math.random variables for damage.
    Couldn't get it to quite work. Function works with hard coded variables
    though.

--- lab/Wk1/js/initial.js
+++ lab/Wk1/js/initial.js
@@ -1,40 +1,54 @@
 //lab1
 //Eddie Gemayel , Tuesday October 1 2013. Duel part 1
 
-
+//player names
 var player1Name = "Spiderman";
 var player2Name = "Batman";
 
+//player healths
 var player1Health = 100;
 var player2Health = 100;
 
+var player1Damage = 10;
+var player2Damage = 5;
 
-
+//round start
 var round = 0;
 
 function fight(){
-	alert(player1Name +":"+ player1Health +"  START  "+ player2Name +":"+player2Health);
+	alert(player1Name +" : "+ player1Health +"  START  "+ player2Name +" : "+player2Health);
 	
-	for(i=0; i<=10; i++){
-		var player1Damage = Math.floor(Math.random());
-		var player2Damage = Math.floor(Math.random());
-		var player1Health =  player1Health - player2Damage;
-		var player2Health = player2Health - player1Damage;
+	for(i=0; i<10; i++){
+	
+		//player1Damage = player1Damage * Math.floor(Math.random()) * 5;
+		//player2Damage = player2Damage *.5;
+		
+		player1Health =  player1Health - player2Damage;
+		player2Health = player2Health - player1Damage;
 		
 		
-	
+		var result = winnerCheck();
+		console.log(winnerCheck());
+		if(result =="No Winner"){
+			round++;
+			alert(player1Name + " : "+ player1Health +"  ROUND "+ round +" OVER" +"  "+ player2Name + " : " + player2Health);
+		}
+		else{
+			alert(result);
+			
+		}
 	}
 	
 	function winnerCheck(){
-		var result ="";
+		var result ="No Winner";
 		
-		if(player1Health > player2Health){
+		if(player2Health < 1){
 			result = player1Name + " WINS!";
 		}
-		else if(player1Health < player2Health){
+		else if(player1Health < 1){
 			result = player2Name + " WINS!";
 		}
-		else if( player1Health < 0 && player2Health < 0){
+		else if( player1Health < 1 && player2Health < 1){
 			result = "You both DIE!";
 		}
 		return result;
@@ -42,4 +56,5 @@ function fight(){
 	
 }
 
+fight();
 
