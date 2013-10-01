c0aa8af50510eb847afe5d8afd1c3dab7eaeee66
eddiegemayel <eddiegem16@gmail.com>
2013-10-01 12:19:23 -0400

    lecture1
    made it a ternary

--- lecture/day1/js/initial.js
+++ lecture/day1/js/initial.js
@@ -4,18 +4,22 @@ var totalSeconds = parseInt(prompt("Enter total seconds: "));
 var min = Math.floor(totalSeconds / 60);
 var sec = totalSeconds % 60;
 
-if(min<10 && sec<10){
-	console.log("0"+ min +" : 0" + sec);
-}
-else if(min>9 && sec<10){
-	console.log(min +" : 0" + sec);
-}
-else if(min>9 && sec>9){
-	console.log(min +" : "+ sec);
-}
-else if (min<10 && sec>9){
-	console.log("0"+ min +" : "+ sec);
-}
+//if(min<10 && sec<10){
+//	console.log("0"+ min +" : 0" + sec);
+//}
+//else if(min>9 && sec<10){
+//	console.log(min +" : 0" + sec);
+//}
+//else if(min>9 && sec>9){
+//	console.log(min +" : "+ sec);
+//}
+//else if (min<10 && sec>9){
+//	console.log("0"+ min +" : "+ sec);
+//}
+
+var str = (min < 10 ? "0" : "") + min + ":" + (sec <10 ? "0" : "") + sec;
+
+console.log("Time : " + str);
 
 //console.log("0"+ min +" : 0" + sec);
 //console.log(min);
