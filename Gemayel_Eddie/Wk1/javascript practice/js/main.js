3b83dbe328b3c1b9f121e764a9b55d05c41fb16a
eddiegemayel <eddiegem16@gmail.com>
2013-10-04 20:01:51 -0400

    Javascript practice
    added comments. done with assignment

--- Gemayel_Eddie/Wk1/javascript practice/js/main.js
+++ Gemayel_Eddie/Wk1/javascript practice/js/main.js
@@ -45,24 +45,24 @@
     console.log('------ Goal2: Assignment: JavaScript Practice ----------');
 
     console.log("1. avg of an array of numbers");
-  	var newArray = [100, 70, 50, 65, 89, 50];
-	var total = 0;
+  	var newArray = [100, 70, 50, 65, 89, 50];	//array with values
+	var total = 0;								//the var that holds the total of all the values
 
 
 	newArray.forEach(function(e){
-		total+=e;
+		total+=e;					//everytime the loop runs, it adds each value from the array together
 
 	});
 
-	var average = total / newArray.length ;
+	var average = total / newArray.length ;		//divides the total by the length of the array to find the average
 
 	console.log(average);
 
     //--------------------------------------------------------|
     console.log("2. concat first and last name");
 
-        function fullName(firstName, lastName){
-        	var name = firstName +" "+ lastName ;
+        function fullName(firstName, lastName){	
+        	var name = firstName +" "+ lastName ;		//function takes the last and first name, concats them together with a space in between as well
         	return name;
         }
 
@@ -73,9 +73,9 @@
     var ipsum = "this is test text that is being used as input to a function";
 
         function wordCount(list){
-        	var count = list.split(" ");
+        	var count = list.split(" ");		//function splits the list by spaces
         	
-        	return count.length;
+        	return count.length;			//this value will tell us how many words are in the sentence
         }
 
     console.log(wordCount(ipsum));
@@ -84,7 +84,7 @@
     console.log("4. sentence char count");
 
         function charCount(words){
-        	var result = words.length;
+        	var result = words.length;		//calculates how many characters are in the function by its length
         	return result;
         }
 
@@ -94,20 +94,20 @@
     console.log("5. how many vowels in a word");
 
         function vowelsInWord(test){
-        	var vowelCount = 0;
+        	var vowelCount = 0;	//this variable holds the number of vowels in the word
         	
         	for(i=0; i<test.length; i++){
         		
-        		var f = test.charAt(i);
+        		var f = test.charAt(i);	//loop through each letter in the string
         		
         		if(f == "a" || f == "e" || f == "i" || f == "o" || f == "u"){
-        			vowelCount++;
+        			vowelCount++;	//if the current letter is a vowel, add 1 to the counter
         		}
         		else{
         		
         		}
         	}
-        	return vowelCount;
+        	return vowelCount;			//return the counter
         }
 
     console.log(vowelsInWord('JavaScript'));
@@ -116,28 +116,28 @@
     console.log("6. find number and create an array of even or odd numbers");
 
         var newArray1 = [];
-		var newArray2 = [];
+		var newArray2 = [];		//these arrays will hold the even and odd numbers
 
 		function findNum(array, f, t){
 	
 	
 	
 			for(i=0; i<array.length; i++){
-				var c = array[i];
-				var b = c % 2;
+				var c = array[i];		//loops through each number in the given array
+				var b = c % 2;			//var b represents the remainder of the number divided by 2
 		
 		
 		
-				if(b==0){
-					f[i]=c;
+				if(b==0){			//if the remainder is 0, it's an even number
+					f[i]=c;			//add to the even number array
 				}
 				else{
-					t[i] =c;
-				}	
+					t[i] =c;		//if the remainder is not 0, its an odd number.
+				}					//add it to the odd number array
 			}
 			
-			return [f, t];
-
+			return [f, t];			//return both arrays to the console log
+	
 		}
 
     console.log(findNum([31,22,4,67,83,6,5,4], newArray1, newArray2));
